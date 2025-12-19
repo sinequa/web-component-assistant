eb56d55950d15340509b0ca3e2d3d46ee6724872
Khoa DO TRINH <kdotrinh@chapsvision.com>
2025-12-19 17:59:22 +0100

    3.10.4

--- src/wrapper/chat-wrapper.ts
+++ src/wrapper/chat-wrapper.ts
@@ -1,6 +1,7 @@
-import { Component, computed, ElementRef, inject, input, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { Component, computed, input, OnDestroy, OnInit, viewChild } from '@angular/core';
 
-import { ChatComponent } from '@sinequa/assistant/chat';
+import { ChatComponent, InitChat, MessageHandler, SuggestedAction } from '@sinequa/assistant/chat';
+import { AppGlobalConfig, Query } from '@sinequa/atomic';
 
 @Component({
   selector: 'sq-chat-wrapper',
@@ -10,8 +11,18 @@ import { ChatComponent } from '@sinequa/assistant/chat';
         [appConfig]="appConfig()!"
         [instanceId]="instanceId()!"
         [query]="query()!"
+        [chat]="chat()"
+        [messageHandlers]="messageHandlers()"
+        [automaticScrollToLastResponse]="automaticScrollToLastResponse()"
+        [assistantMessageIcon]="assistantMessageIcon()"
+        [userMessageIcon]="userMessageIcon()"
+        [connectionErrorMessageIcon]="connectionErrorMessageIcon()"
+        [searchWarningMessageIcon]="searchWarningMessageIcon()"
+        [additionalWorkflowProperties]="additionalWorkflowProperties()"
+
         (openDocument)="onOpenDocument($event)"
         (openPreview)="onOpenPreview($event)"
+        (suggestAction)="onSuggestAction($event)"
       />
     }
   `,
@@ -19,9 +30,18 @@ import { ChatComponent } from '@sinequa/assistant/chat';
   imports: [ChatComponent],
 })
 export class ChatWrapperComponent implements OnInit, OnDestroy {
-  appConfig = input<any>();
+  appConfig = input<AppGlobalConfig>();
   instanceId = input<string>();
-  query = input<any>();
+  query = input<Query>();
+  chat = input<InitChat>();
+  messageHandlers = input< Map<string, MessageHandler<any>>>(new Map());
+  automaticScrollToLastResponse = input<boolean>(false);
+  assistantMessageIcon = input<string>('sq-sinequa');
+  userMessageIcon = input<string>('');
+  connectionErrorMessageIcon = input<string>('');
+  searchWarningMessageIcon = input<string>('');
+  additionalWorkflowProperties = input<Record<string, any>>({});
+
   sqChat = viewChild.required('sqChat', {read: ChatComponent});
 
   isReady = computed(
@@ -49,4 +69,29 @@ export class ChatWrapperComponent implements OnInit, OnDestroy {
     document.dispatchEvent(
       new CustomEvent('openDocument', { detail: event })
     );
+
+  onSuggestAction(action: SuggestedAction) {
+      switch (action.type) {
+        case "Prefill":
+          this.insertText(action.content);
+          break;
+        case "Submit":
+          this.submitQuestion(action.content);
+          break;
+        default:
+          console.log("Assistant's unknown suggested action type: " + action.type);
+      }
+    }
+
+    submitQuestion(question: string) {
+        this.sqChat().question = question;
+        this.sqChat().submitQuestion();
+    }
+
+    insertText(text: string): void {
+      const start = this.sqChat().questionInput!.nativeElement.selectionStart;
+      const end = this.sqChat().questionInput!.nativeElement.selectionEnd;
+      this.sqChat().question = this.sqChat().question.substring(0, start) + text + this.sqChat().question.substring(end, this.sqChat().question.length);
+      this.sqChat().questionInput!.nativeElement.value = this.sqChat().question;
+    }
 }
